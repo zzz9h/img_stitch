25647adaa46f4283936fef3968f511538a7a56f5
zzz9h <45917646+zzz9h@users.noreply.github.com>
2020-11-17 16:11:14 +0800

    Add files via upload

---
+++ CMakeFiles/ShowImage.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ShowImage.dir/ShowImage.cpp.o"
+  "ShowImage.pdb"
+  "ShowImage"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ShowImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ run.sh
@@ -0,0 +1,3 @@
+cmake .
+make
+./ShowImage --mode scans 1.jpg 2.jpg 3.jpg 4.jpg
